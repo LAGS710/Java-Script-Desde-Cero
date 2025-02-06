ffb1bc229df1c9e067a9ffd5fdc45def967c3ab2
LAGS710 <luisgonzales2_hotmail.com>
2025-02-05 19:01:45 -0600

    Introducción a JavaScript y su Importancia

---
+++ 1-fundamentos-js/tipos-de-datos.js
@@ -0,0 +1,11 @@
+
+console.log('el tipo de dato de "redonda" es', typeof "redonda");
+console.log('el tipo de dato de "42" es', typeof 42);
+console.log('el tipo de dato de "Veinticinco" es', typeof 'Veinticinco');
+console.log('el tipo de dato de "-666" es', typeof -666);
+console.log('el tipo de dato de "true" es', typeof true);
+console.log('el tipo de dato de "0" es', typeof 0);
+console.log('el tipo de dato de "null" es', typeof null);
+console.log('el tipo de dato de "undefined" es', typeof undefined);
+console.log('el tipo de dato de "FALSE" es', typeof FALSE);
+console.log("el tipo de dato de '' es", typeof '');
